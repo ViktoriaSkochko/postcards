0bfaa4975c50ae912e5de466ced34854b10fd799
Viktoriia Skochko <skochko.viktoriia@gmail.com>
2020-03-07 21:42:42 +0200

    create navigation menu

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationMenuComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
+  ],
+  exports: [
+    NavigationMenuComponent
   ]
 })
 export class NavigationModule { }
