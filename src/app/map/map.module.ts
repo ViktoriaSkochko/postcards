712b4271f8e06ad460adfd296f7aaf92534ccb8a
Viktoriia Skochko <skochko.viktoriia@gmail.com>
2020-03-08 20:15:15 +0200

    add routing to map module

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapContainerComponent } from './map-container/map-container.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const mapRoutes: Routes = [
+  { path: '',  component: MapContainerComponent }
+];
 
 @NgModule({
   declarations: [MapContainerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(mapRoutes)
   ]
 })
 export class MapModule { }
