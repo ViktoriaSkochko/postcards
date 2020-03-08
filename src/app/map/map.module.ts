05da10c88ca9c0c1b28e16d1abb52c8eea90e066
Viktoriia Skochko <skochko.viktoriia@gmail.com>
2020-03-08 19:51:46 +0200

    create map (module) and map-container (component)

---
+++ src/app/map/map.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MapContainerComponent } from './map-container/map-container.component';
+
+
+
+@NgModule({
+  declarations: [MapContainerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MapModule { }
