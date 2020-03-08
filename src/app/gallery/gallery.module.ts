4d817057518ccef38fd8fcc489b3f80061eec60b
Viktoriia Skochko <skochko.viktoriia@gmail.com>
2020-03-08 20:50:00 +0200

    create gallery and statistics modules and components

---
+++ src/app/gallery/gallery.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GalleryContainerComponent } from './gallery-container/gallery-container.component';
+
+
+
+@NgModule({
+  declarations: [GalleryContainerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GalleryModule { }
---
+++ src/app/statistics/statistics.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StatisticsContainerComponent } from './statistics-container/statistics-container.component';
+
+
+
+@NgModule({
+  declarations: [StatisticsContainerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class StatisticsModule { }
