3aad02facd8d31985bebffb843ddfb984f3a3054
Viktoriia Skochko <skochko.viktoriia@gmail.com>
2020-03-08 21:19:38 +0200

    add routing to gallery module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path: 'map',
     loadChildren: () => import('./map/map.module').then(m => m.MapModule)
+  },
+  {
+    path: 'gallery',
+    loadChildren: () => import('./gallery/gallery.module').then(m => m.GalleryModule)
   }
 ];
 
--- src/app/gallery/gallery.module.ts
+++ src/app/gallery/gallery.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GalleryContainerComponent } from './gallery-container/gallery-container.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const galleryRoutes: Routes = [
+  { path: '',  component: GalleryContainerComponent }
+];
 
 @NgModule({
   declarations: [GalleryContainerComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(galleryRoutes)
   ]
 })
 export class GalleryModule { }
