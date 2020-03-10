f4dfda8b7086deffd13f7db45bfe9576944aa897
Viktoriia Skochko <skochko.viktoriia@gmail.com>
2020-03-10 21:52:47 +0200

    add navigation links to postcards

--- src/app/my-postcards/my-postcards.module.ts
+++ src/app/my-postcards/my-postcards.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SentComponent } from './sent/sent.component';
 import { ReceivedComponent } from './received/received.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const myPostcardsRoutes: Routes = [
+  { path: 'sent',  component: SentComponent },
+  { path: 'received',  component: ReceivedComponent }
+];
 
 @NgModule({
   declarations: [SentComponent, ReceivedComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(myPostcardsRoutes)
   ]
 })
 export class MyPostcardsModule { }
